16047b42d975dde5438060dba705df5db7434e5a
權少 <ljqican@163.com>
2023-10-28 10:34:41 +0800

    嘗試開始重新變更包功能。

--- current.ps1
+++ current.ps1
@@ -1,4 +1,4 @@
-﻿# 返回當前狀態
+﻿# 將當前狀態輸出到文件
 
 # 默認當前目錄
 $path = $PWD
@@ -7,7 +7,23 @@ if ($args.Length -ge 1)
     $path = $args[0]
 }
 
+$dest = ""
+if ($args.Length -ge 2)
+{
+    $dest = $args[1]
+}
+
+function max($a, $b) {
+    if ($a -gt $b) {
+        return $a
+    }
+    else {
+        return $b
+    }
+}
+
 $set = @{};
+$lastWrite = $null
 Get-ChildItem -Path $path -Recurse
 | ForEach-Object {
     $isDir = $false
@@ -16,6 +32,24 @@ Get-ChildItem -Path $path -Recurse
         $isDir = $true
     }
     $set[$_.FullName] = $isDir
+    if ($lastWrite -eq $null) {
+        $lastWrite = $_.LastWriteTime
+    }
+    else {
+        $lastWrite = max $lastWrite $_.LastWriteTime
+    }
 }
 
-return $set
+# 寫入到本地目錄
+if ($dest -ne "") {
+    $set
+    | ConvertTo-Json
+    | Out-File -Encoding utf8BOM (Join-Path $dest "state.json")
+
+    # [System.Convert]::ToDateTime(
+    $lastWrite.ToString()
+    | Out-File -Encoding utf8BOM (Join-Path $dest "LastWriteTime.txt")
+}
+
+# 返回文件列表和最後修改時間
+return $set, $lastWrite
---
+++ diff.ps1
@@ -0,0 +1,68 @@
+﻿# 複製給定日期後有修改的文件
+
+$path = $PWD
+
+if ($args.Length -lt 2)
+{
+    exit 1
+}
+
+$package = $args[0] # 狀態記錄
+$target = $args[1] # 目標路徑
+
+$parent = (Get-Item $target).Parent
+
+if ($args.Length -ge 3)
+{
+    $path = $args[2]
+}
+
+if (Test-Path $target -PathType Container)
+{
+    Remove-Item $target -Recurse
+}
+
+New-Item $target -ItemType Directory
+
+$zip = Join-Path $parent.ToString() "update.zip"
+
+if (Test-Path $zip -PathType Leaf)
+{
+    Remove-Item $zip
+}
+
+# 求出狀態
+$json = (Get-Content (Join-Path $package "state.json") -Raw | ConvertFrom-Json).psobject.properties
+
+$state = @{}
+foreach ($i in $json) {
+    $state[$i.Name] = $i.Value
+}
+
+# 求出日期
+$dateStr = Get-Content -Raw (Join-Path $package "LastWriteTime.txt")
+$date = [System.Convert]::ToDateTime($dateStr)
+
+# Get-ChildItem -Path $path -Recurse
+# | Where-Object { $_.LastWriteTime -gt $date }
+# | ForEach-Object {
+#     $relative = [IO.Path]::GetRelativePath($pwd, $_.FullName)
+#     $dest = Join-Path $target "update" $relative
+#     Copy-Item -Path $_.FullName -Destination $dest -Force
+# }
+
+# $compress = @{
+#     Path = "$target/*.*"
+#     DestinationPath = $zip
+#     CompressionLevel = "Fastest"
+# }
+# Compress-Archive @compress
+
+
+
+# return ($state, $date)
+Write-Output $state
+
+Write-Output "----------------------------------------------------"
+
+Write-Output $date
