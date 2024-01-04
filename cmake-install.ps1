425e5b809f654a3be3e5da1772bb518419662126
權少 <ljqican@163.com>
2024-01-04 22:30:12 +0800

    添加CMake編譯安裝庫腳本。

---
+++ cmake-install.ps1
@@ -0,0 +1,28 @@
+﻿$config = $args[0]
+
+if (("Debug", "Release") -notcontains $config) {
+    Write-Host "未知配置"
+    exit 1
+}
+
+function build($config) {
+    cmake --build build --config $config
+}
+
+function install($config) {
+    gsudo cmake --build build --target install --config $config
+}
+
+build $config
+if ($LASTEXITCODE -ne 0) {
+    exit 1
+}
+Write-Output "構建成功"
+
+install $config
+if ($LASTEXITCODE -ne 0) {
+    exit 1
+}
+Write-Output "安裝成功"
+
+exit 0
