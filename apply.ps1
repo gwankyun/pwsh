7af551f0b1f0a310715cd61e19fe32b605a90e3f
權少 <ljqican@163.com>
2024-01-08 23:28:44 +0800

    apply.ps1腳本優化。

--- apply.ps1
+++ apply.ps1
@@ -6,7 +6,7 @@ $json = get_config
 if ($json -and $json.path -and (Test-Path $json.path)) {
     $path = $json.path
 }
-Write-Output ("path: " + $path)
+Write-Host ("path: " + $path)
 
 # 是否重置時間
 [bool]$ignore_date = $false
@@ -30,14 +30,14 @@ if ($patch.Length -eq 0)
     if ($path -ne "") {
         Rar x ($path + $file) $h
         if ($LASTEXITCODE -ne 0) {
-            Write-Output "解縮失敗"
+            Write-Host "解縮失敗"
             exit 1
         }
     }
 }
 
 if (-not (Test-Path *.patch)) {
-    Write-Output "找不到patch文件"
+    Write-Host "找不到patch文件"
     exit 1
 }
 
@@ -55,10 +55,22 @@ if ($args.Contains("-i") -or $ignore_date)
 
 $patch = Get-ChildItem *.patch
 
-$r = Start-Process -FilePath git -ArgumentList "${am} ${patch}" -NoNewWindow -Wait -PassThru
-Write-Output $r.ExitCode
+# $r = Start-Process -FilePath git -ArgumentList "${am} ${patch}" -NoNewWindow -Wait -PassThru
+# Write-Output $r.ExitCode
+
+git $am (Get-ChildItem *.patch)
+Write-Host $LASTEXITCODE
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "合併成功"
+}
 
 # 暫停一秒
 Start-Sleep -Seconds 1
 
 Remove-Item *.patch
+
+if ($LASTEXITCODE -ne 0) {
+    exit 1
+}
+
+exit 0
