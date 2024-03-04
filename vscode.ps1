9f5037861068553bd67f79b1d48705a72fcc8d6a
權少 <ljqican@163.com>
2024-03-05 00:11:58 +0800

    添加生成VSCode擴展功能。

---
+++ vscode.ps1
@@ -0,0 +1,14 @@
+﻿Write-Host $args
+$a, $b = ($args[0]).Split(".")
+$ver = $args[1]
+
+$url = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$a/vsextensions/$b/$ver/vspackage"
+
+if ($args.Length -ge 3) {
+    if ($args[2] -ne "false") {
+        $url = "${url}?targetPlatform=win32-x64"
+    }
+}
+
+Set-Clipboard $url
+Write-Host $url
