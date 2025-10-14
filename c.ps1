ce8765b84b43f0f1da2d8b964ac444f93504ebb1
權少 <ljqican@163.com>
2025-10-14 22:09:26 +0800

    添加cmake命令簡化腳本。

---
+++ c.ps1
@@ -0,0 +1,20 @@
+﻿$preset = ""
+$mode = "config"
+
+if ($args.Count -ge 1) {
+    $preset = $args[0]
+}
+
+if ($args.Count -ge 2) {
+    $c = $args[1]
+    if ($c -eq "b") {
+        $mode = "build"
+    }
+}
+
+if ($mode -eq "build") {
+    cmake --build --preset=$preset
+}
+else {
+    cmake --preset=$preset
+}
