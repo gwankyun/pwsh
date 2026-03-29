fa6dcc02ede72aa5f808375565735ca67f5cad9a
權少 <ljqican@163.com>
2026-03-29 23:29:46 +0800

    添加合併腳本。

--- Merge-StatusDiff.ps1
+++ Merge-StatusDiff.ps1
@@ -13,15 +13,50 @@ $compare = Get-Content -Path $compare_path -Raw -Encoding utf8BOM
 
 foreach ($i in $compare) {
     Write-Output $i
+    # 是否文件夾
+    $isContainer = $i.PSIsContainer
+    # 目標路徑
+    $target = Join-Path $path $i.Relative -Resolve
+    # 源路徑
+    $file = Join-Path $diff "data" $i.Relative
     switch ($i.Status) {
         "creation" {
-            $rela = Join-Path $path $i.Relative -Resolve
-            Write-Output $rela.ToString()
+            if ($isContainer) {
+                # 目錄就創建
+                New-Item -Path $target -ItemType Directory -Force
+            } else {
+                # 文件就複製
+                Copy-Item -Path $file -Destination $target -Force
+            }
         }
-        "deletion" { }
+        "deletion" {
+            # 直接刪除，無論是文件還是文件夾
+            if (Test-Path -Path $target -PathType Container) {
+                Remove-Item -Path $target -Recurse -Force
+            } else {
+                Remove-Item -Path $target -Force
+            }
+        }
         "modification" {
-            $rela = Join-Path $path $i.Relative -Resolve
-            Write-Output $rela.ToString()
+            $targetIsContainer = Test-Path -Path $target -PathType Container
+            if ($isContainer) {
+                if ($targetIsContainer) {
+                    throw "PSIsContainer" # 已經過濾了，不可能
+                } else { # 目標是文件
+                    # 先刪文件再建文件夾
+                    Remove-Item -Path $target -Force
+                    New-Item -Path $target -ItemType Directory -Force
+                }
+            } else {
+                if ($targetIsContainer) { # 目標是文件夾
+                    # 先刪文件夾再複製文件
+                     Remove-Item -Path $target -Recurse -Force
+                     Copy-Item -Path $file -Destination $target -Force
+                } else { # 目標是文件
+                    Remove-Item -Path $target -Force
+                    Copy-Item -Path $file -Destination $target -Force
+                }
+            }
         }
         default {
             throw "Status"
